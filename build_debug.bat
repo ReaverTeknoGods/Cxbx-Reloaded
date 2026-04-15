073d5d15e488c23433f96030c2546668b876d3f0
ReaverTeknoGods <c0de187nig@gmail.com>
2026-04-15 06:27:47 +0400

    add debug build bat

---
+++ build_debug.bat
@@ -0,0 +1,24 @@
+@ECHO off
+SETLOCAL
+
+:: Generate build files if not already present
+IF NOT EXIST "build\Cxbx-Reloaded.sln" (
+    ECHO Generating Visual Studio project files...
+    cmake -B build -G "Visual Studio 17 2022" -A Win32
+    IF ERRORLEVEL 1 (
+        ECHO CMake generation failed!
+        EXIT /B 1
+    )
+)
+
+SET CONFIG=Debug
+
+ECHO Building %CONFIG%...
+cmake --build build --config %CONFIG% -- /m
+IF ERRORLEVEL 1 (
+    ECHO Build failed!
+    EXIT /B 1
+)
+
+ECHO.
+ECHO Build successful! Output in build\bin\%CONFIG%\
